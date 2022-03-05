44c5ff3cc281e7bb2d4c9c8ce794fefe42e281b3
Samuel <samuel.ward@outlook.fr>
2022-03-05 13:57:16 +0100

    test

--- src/components/EventAdd.js
+++ src/components/EventAdd.js
@@ -1,14 +1,27 @@
+import "../styles/EventAdd.scss";
+
 export const EventAdd = () => {
 	return (
-		<section>
+		<section id="eventAdd">
 			<h2 className="section-title">Ajouter un événement</h2>
-			<form method="POST" action="/api/events/add">
-				<input type="text" placeholder="Nom" name="eventName" required/>
-				<input type="datetime-local" name="eventDateTime" required/>
-				<input type="text" placeholder="Description" name="eventDescription" required/>
-				<input type="text" placeholder="Location" name="eventLocation" required/>
-				<input type="submit"/>
-			</form>
+			<div id="eventAdd-container">
+				<div id="eventAdd-title">
+					<h2>Evenement</h2>
+				</div>
+				<div id="eventAdd-details">
+					<form method="POST" action="/api/events/add">
+						<label for="eventName">Nom</label>
+						<input className="user-input" type="text" placeholder="Soirée, bowling..." name="eventName" required/>
+						<label for="eventDateTime">Date & heure</label>
+						<input className="user-input" type="datetime-local" name="eventDateTime" required/>
+						<label for="eventDescription">Description</label>
+						<input className="user-input" type="textarea" placeholder="Lorem ipsum..." name="eventDescription" required/>
+						<label for="eventLocation">Endroit</label>
+						<input className="user-input" type="text" placeholder="Rouen, en ligne..." name="eventLocation" required/>
+						<button className="user-input" type="submit">Ajouter l'événement</button>
+					</form>
+				</div>
+			</div>
 		</section>
 	);
 }
